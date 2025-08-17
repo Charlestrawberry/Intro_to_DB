a4c804951a4f8f3e7cf461a16158e0a653098dee
Oduks <charlestrawb_odus@yahoo.com>
2025-08-17 22:24:59 +0100

    fixing the tables

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+-- Print the full description of the BOOKS table
+-- Without using DESCRIBE or EXPLAIN
+
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE()
+  AND TABLE_NAME = 'books';
