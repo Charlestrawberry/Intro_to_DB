4d840cbbc8d54899b2929d789e59c68fe3f2dbe6
Oduks <charlestrawb_odus@yahoo.com>
2025-08-17 22:32:56 +0100

    task 4

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,5 @@
 -- Use the correct database
 USE alx_book_store;
 
--- Show the full description of the BOOKS table without DESCRIBE or ANALYZE
+-- Print the full description of the BOOKS table
 SHOW CREATE TABLE BOOKS;
