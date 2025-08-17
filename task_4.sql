cc35cd445a1c46e15aad11b9a822f689295b93c2
Oduks <charlestrawb_odus@yahoo.com>
2025-08-17 23:24:13 +0100

    fixed script

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,8 @@
--- Use the correct database
+-- Use the alx_book_store database
 USE alx_book_store;
 
--- Get the full description of the BOOKS table without DESCRIBE or EXPLAIN
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
+-- Print the full description of the BOOKS table without using DESCRIBE or EXPLAIN
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'BOOKS';
+  AND TABLE_NAME = 'Books';
