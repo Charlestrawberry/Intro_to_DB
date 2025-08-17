174879c5abead3f802923ecc2cf63d0aa34ce78b
Oduks <charlestrawb_odus@yahoo.com>
2025-08-17 23:29:20 +0100

    fix bugs

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,12 @@
--- Use the alx_book_store database
-USE alx_book_store;
-
--- Print the full description of the BOOKS table without using DESCRIBE or EXPLAIN
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'Books';
+SELECT 
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
